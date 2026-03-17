87de599412fcae11c5dabb8c3c6133345fec88a7
joshkoma <joshkoma@gmail.com>
2026-03-17 17:58:03 +0300

    added assets

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const mobileMenuBtn = document.getElementById('mobileMenuBtn');
 const mobileDrawer = document.getElementById('mobileDrawer');
+const dropdown = document.querySelector('.nav-item-dropdown');
+const dropdownToggle = document.querySelector('.dropdown-toggle');
 
 if (mobileMenuBtn && mobileDrawer) {
   mobileMenuBtn.addEventListener('click', () => {
     mobileDrawer.classList.toggle('open');
+  });
+}
+
+
+if (dropdown && dropdownToggle) {
+  dropdownToggle.addEventListener('click', function (e) {
+    e.preventDefault();
+    dropdown.classList.toggle('open');
+  });
+
+  document.addEventListener('click', function (e) {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove('open');
+    }
   });
 }
